8ce99c35103236eb78ffee8057012f8159e4b41d
Kaniel D. Tapper <kanieldannn@gmail.com>
2023-04-13 16:26:27 -0400

    second

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    const githubURL = "https://github.com/kanielDT/react-counter-app"
+    
+    return (
+        <footer>
+            <p><small>&copy; 2023</small><br />
+            <a href={"githubURL"} target="_blank" rel="noreferrer"></a></p>
+        </footer>
+    )
+}
